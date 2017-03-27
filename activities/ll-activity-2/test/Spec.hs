9df947486373292abbda85c2cc4052a146c1dc4a
Mattox Beckman <mattox@illinois.edu>
2017-03-27 12:34:06 -0500

    Regraded LL activity with better tests.

--- activities/ll-activity-2/test/Spec.hs
+++ activities/ll-activity-2/test/Spec.hs
@@ -1,47 +1,3 @@
--- import Test.Framework (defaultMain, testGroup)
--- import Test.Framework.Providers.QuickCheck2 (testProperty)
--- import Lib
--- import Test.QuickCheck
---
--- main :: IO ()
--- main = defaultMain tests
---
--- -- data Exp = PlusExp Exp Exp
--- --          | IntExp Integer
--- --          | VarExp String
--- --          | LetExp String Exp Exp
--- --     deriving Show
---
---
--- tests = [
---         testGroup "Display Function" [
---             testProperty "Parser works for small expressions." (forAll (arbExp 0) displayParses)
---             , testProperty "Parser works for large expressions." (forAll arbitrary displayParses)
--- --                testProperty "Length is preserved" (forAll randHydra sameLength)
--- --           ,    testProperty "Difference is correct" (forAll randHydra difference)
---            ]
---       ]
---
--- instance Arbitrary Exp where
---    arbitrary = sized arbExp
---
--- arbExp :: Int -> Gen Exp
--- arbExp 0 = do
---     frequency [(1, IntExp <$> arbitrary)
---               ,(1, VarExp <$> varName)]
---
--- arbExp n = do
---   frequency [(1, PlusExp <$> arbExp (n-1) <*> arbExp 0)
---             ,(1, PlusExp <$> arbExp 0 <*> arbExp (n-1))
---             ,(1, LetExp <$> varName <*> arbExp (n-1) <*> arbExp 0)
---             ,(1, LetExp <$> varName <*> arbExp 0 <*> arbExp (n-1))
---             ]
---
--- varName = resize 4 (listOf1 $ elements ['a'..'z'])
---
--- displayParses exp =
---   let (e1,_) = parse (display exp)
---     in e1 == exp
 import Test.Framework (defaultMain, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Lib
@@ -98,11 +54,11 @@ displayParsesParen exp = monadicIO $ do
 xkcd :: Int
 xkcd = 4
 
+range1_xkcd :: Gen Int
+range1_xkcd = resize 1 $ choose (1::Int,xkcd)
+
 coinFlip :: IO Bool
-coinFlip = generate (resize 1 $ choose (1::Int,xkcd)) >>= (\x ->
-  if (x>2) then
-    return True
-    else return False)
+coinFlip = generate range1_xkcd >>= (\x -> return (x>2))
 
 parify :: String -> IO String
 parify s = coinFlip >>= (\b -> if b then return (" ( " ++ s ++ " ) ") else return s)
