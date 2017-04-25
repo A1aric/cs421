dd2ce98c25817b01fc0b4f0f71b081a5186e13da
Sujay <cheeselover724@gmail.com>
2017-04-24 22:34:38 -0500

    done with mp5 maybe with lots of help

--- mps/mp5-parser/src/Lib.hs
+++ mps/mp5-parser/src/Lib.hs
@@ -68,32 +68,49 @@ ident = do i <- try $ do ii <- many1 (oneOf (['a'..'z'] ++ ['A'..'Z'])) <?> "an 
                          if ii == "eps" then fail "eps not expected" else return ii
            _ <- inlinews
            return i
+-- solutions taken from mattox
 
 -- realIdent - like ident, but returns it as a Symbol
 realIdent :: Parser Symbol
-realIdent = undefined
+realIdent = do t <- ident <?> "an identifier"
+               _ <- inlinews
+               return (Symbol t)
 
 -- epsilon - parse "eps" or "ε" returning Epsilon
 epsilon :: Parser Symbol
-epsilon = undefined
+epsilon = do _ <- (string "ε" <|> string "eps") <?> "an epsilon"
+             _ <- inlinews
+             return Epsilon
 
 -- epsilonLine - parse a production that only has an epsilon in it.
 epsilonLine :: Parser [Symbol]
-epsilonLine = undefined
+epsilonLine = do _ <- epsilon
+                 _ <- endOfLine
+                 return [Epsilon]
 
 -- tokenLine - parse a production that is not an epsilon.
 tokenLine :: Parser [Symbol]
-tokenLine = undefined
+tokenLine = do tt <- many1 realIdent
+               _ <- endOfLine
+               return tt
 
 -- initialProduction - parse an initial production line
 initialProduction :: Parser (String,[Symbol])
-initialProduction = undefined
+initialProduction = do s <- ident
+                       _ <- stringws "->"
+                       tt <- epsilonLine <|> tokenLine
+                       return (s,tt)
 
 continueProduction :: Parser [Symbol]
-continueProduction = undefined
+continueProduction = do try $ do _ <- inlinews
+                                 stringws "|"
+                        res <- epsilonLine <|> tokenLine
+                        return res
 
 production :: Parser Production
-production = undefined
+production = do (s,x) <- initialProduction
+                xs <- many continueProduction
+                return (Production s (x:xs))
 
 grammar :: Parser Grammar
 grammar = do p <- many1 production
@@ -105,13 +122,14 @@ p1 = "S -> x S y\n | z q Y\nY -> x Y y \n| eps\n"
 -- Some analysis
 
 nonTerminals :: [Production] -> S.HashSet Symbol
-nonTerminals _ = S.empty
+nonTerminals g = S.fromList $ map (\ (Production s _) -> Symbol s) g
 
 symbols :: [Production] -> S.HashSet Symbol
-symbols _ = S.empty
+symbols [] = S.empty
+symbols ((Production s xx):ps) = S.union (S.insert (Symbol s) $ S.fromList (concat xx)) (symbols ps)
 
 terminals :: [Production] -> S.HashSet Symbol
-terminals _ = S.empty
+terminals g = S.difference (symbols g) (nonTerminals g)
 
 fix f x =
   if x == result
@@ -119,17 +137,66 @@ fix f x =
     else fix f result
   where result = f x
 
+-- worked with hagarwa3 and geva2
 -- getFirstSet grammar
 -- calculate the first sets of the nonterminals in a grammar
 getFirstSet :: Grammar -> H.HashMap Symbol (S.HashSet Symbol)
-getFirstSet = undefined
+getFirstSet (Grammar psets terminals nonterminals) =
+    fix aux initial
+    where   initial = H.fromList (zip (S.toList nonterminals) (repeat S.empty))
+            aux fs = H.union (getHelper psets fs) fs
 
+
+getHelper :: [Production] -> H.HashMap Symbol (S.HashSet Symbol) -> H.HashMap Symbol (S.HashSet Symbol)
+getHelper   ((Production s xx):ps)   res =
+    let xs = (map (\v -> head v) xx)
+    in  getHelper (ps) (H.insert (Symbol s) (S.union (S.fromList xs) (firsthelpermerge res xx)) (res))
+getHelper   []                       res = res
+
+firsthelpermerge :: H.HashMap Symbol (S.HashSet Symbol) -> [[Symbol]] -> S.HashSet Symbol
+firsthelpermerge result (x:xs) = S.union (firsthelpermerge result xs) (first result x)
+firsthelpermerge _ [] = S.empty
+
+-- worked with hagarwa3 and geva2
 -- first fs symbols
 -- return the first set of a set of symbols
 first :: H.HashMap Symbol (S.HashSet Symbol) -> [Symbol] -> S.HashSet Symbol
-first  = undefined
+first fs [] = S.empty
+first fs xx = S.fromList $ (map (\v ->  if v == "eps" || v == "ε"
+                                        then Epsilon
+                                        else Symbol v) $ firstHelper fs xx [])
 
+firstHelper :: H.HashMap Symbol (S.HashSet Symbol) -> [Symbol] -> [String] -> [String]
+firstHelper fs (x:xs) res =
+    let a = H.lookup x fs
+    in  case a of
+            Just b   -> if S.member Epsilon b
+                        then (firstHelper fs (xs) res) ++ [show x] ++ (map show $ S.toList (S.delete Epsilon b))
+                        else res ++ [show x] ++ (map show $ S.toList b)
+            Nothing  -> (res ++ [show x])
+
+firstHelper fs []     res = res ++ ["eps"]
+
+-- worked with hagarwa3 and geva2
 -- isLL
 isLL :: Grammar -> Bool
-isLL g = undefined
+isLL g = (not (isLeftRecursive g)) && (hasnoCommonPrefix g)
 
+isLeftRecursive :: Grammar -> Bool
+isLeftRecursive g = checkMap (H.toList fs)
+        where fs = getFirstSet g
+
+checkMap :: [(Symbol, S.HashSet Symbol)] -> Bool
+checkMap ((k,v):fset)   = (S.member k v) || (checkMap fset)
+checkMap []             = False
+
+-- worked with mostofi2
+hasnoCommonPrefix :: Grammar -> Bool
+hasnoCommonPrefix (Grammar psets terminals nonterminals) =
+    let    fs = (getFirstSet (Grammar psets terminals nonterminals))
+    in     checkc fs psets True
+    where
+        checkc fs []                        b = b
+        checkc fs ((Production s xx):xs)    b = b && ((unionLen fs xx) == (listLen fs xx)) && (checkc fs xs b)
+        unionLen    fs sets = S.size (S.unions (map (\x -> first fs x) sets))
+        listLen     fs sets = foldr (\a b -> b + (S.size (first fs a))) 0 sets
